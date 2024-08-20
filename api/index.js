9043a607e93c75c7374c5d8c2a48f9699c75633a
AmanYize <amanexo19@gmail.com>
2024-08-20 18:10:28 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import "dotenv/config";
+mongoose
+  .connect(process.env.MONGOSECRET)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log("Server started on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const app = express();
-
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
